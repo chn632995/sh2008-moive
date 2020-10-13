11c3f6e31c9cfc9b009011d24de44c40dab87c53
CherishYii <cherish@cherish.pw>
2020-10-14 01:45:07 +0800

    登录

--- src/router/routes/auth.js
+++ src/router/routes/auth.js
@@ -3,8 +3,5 @@ export default [
         path: "/login",
         component: () => import("@/views/Auth/Login"),
     },
-    {
-        path: "/logout",
-        component: () => import("@/views/Auth/Logout"),
-    },
+    //...
 ];
